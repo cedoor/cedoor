93755a4a2e2fefe6b90ed52c273f7cf4d06d4979
Cedoor <me@cedoor.dev>
2026-07-29 12:30:21 +0200

    feat(neovim): enable global wrap and linebreak for long text

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -2,3 +2,7 @@ vim.g.mapleader = ","
 
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Testo lungo: wrap globale
+vim.opt.wrap = true
+vim.opt.linebreak = true
